3eef0b2487f85fec6129995f4ae127996bddaa44
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-28 17:42:49 -0300

    buiu

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação usada para criar interatividade em páginas web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente?',
+    'O maior continente do mundo é a Ásia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'Um loop é uma estrutura de controle que repete um bloco de código enquanto uma condição é verdadeira.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz comer em inglês?',
+    'Comer em inglês é "eat".'
+)
